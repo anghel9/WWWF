7acc5db799eb5ff49715e166cd79c3fb5cb091e4
nriddell00 <nriddell35@gmail.com>
2024-12-01 22:04:54 -0800

    enables viewbindings and set up inflator in MainActivity.java

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.groupproject;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import com.example.groupproject.databinding.ActivityMainBinding;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,16 +10,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
