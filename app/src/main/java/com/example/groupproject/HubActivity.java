0a62a25417f5aebfc9fc808d7d25082831a90614
CarkinOtter <carcardoza@csumb.edu>
2024-12-02 17:53:15 -0800

    :tada: added BattleActivity HubActivity StatsActivity WorldActivity

--- app/src/main/java/com/example/groupproject/HubActivity.java
+++ app/src/main/java/com/example/groupproject/HubActivity.java
@@ -1,7 +1,14 @@
 package com.example.groupproject;
 
 import android.os.Bundle;
-
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.annotation.TargetApi;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +16,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HubActivity extends AppCompatActivity {
+
+    private WebView mWebview ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,30 @@ public class HubActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        mWebview .loadUrl(getString(R.string.WebviewURL));
+        setContentView(mWebview );
+
     }
+
 }
