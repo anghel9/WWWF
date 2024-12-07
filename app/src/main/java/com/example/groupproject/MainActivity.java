df1318b241016aa2d3e32025d1315ff29a72f598
Jacob Zamora <113962665+JZmor@users.noreply.github.com>
2024-12-06 16:14:30 -0800

    Hub activity + switching between main & login implemented

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.groupproject;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.groupproject.R;
+import com.example.groupproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,11 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = " A";
 
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        binding.actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //FOR TESTING
+                //startActivity(HubActivity.hubActivityIntentFactory(getApplicationContext()));
+                startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
+            }
+        });
     }
     static Intent mainActivityIntentFactory(Context context, int userId) {
         Intent intent = new Intent(context, MainActivity.class);
